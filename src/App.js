e9499a8d4d652243ab28daef8e05324558428d92
Mohityadav03-Git <mohitcode29@gmail.com>
2022-02-21 17:32:10 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navigation from "./navigation";
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        {/* calling component which shows our homepage*/}
+
+        <Navigation />
+      </div>
+    </>
+  );
+}
+
+export default App;
+// expoted in index.js
